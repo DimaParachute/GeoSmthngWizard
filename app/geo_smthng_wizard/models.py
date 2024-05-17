8451a0135a1574388470a0c19c1476e423adda5a
Dmitriy Fetyukhin <DFetyukhin@alfabank.ru>
2024-05-17 09:46:39 +0300

    Добавлен фильтр для поиска объектов внутри Bbox

--- app/geo_smthng_wizard/models.py
+++ app/geo_smthng_wizard/models.py
@@ -2,7 +2,7 @@ from django.contrib.gis.db import models
 
 class Country(models.Model):
     name = models.TextField(max_length=255)
-    location = models.PointField()
+    geom = models.PolygonField()
 
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class City(models.Model):
     description = models.TextField(max_length=1024)
     photo = models.TextField(max_length=255)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    location = models.PointField()
+    geom = models.PointField()
 
     def __str__(self):
         return self.name
@@ -20,7 +20,7 @@ class City(models.Model):
 class Capital(models.Model):
     name = models.TextField(max_length=255)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    location = models.PointField()
+    geom = models.PointField()
 
     def __str__(self):
         return self.name
